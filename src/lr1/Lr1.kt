f93be55ace69921782d671517a2a5b3efbaf3131
Yaroslav <syrovatkaaroslav@gmail.com>
2024-03-31 17:42:48 +0300

    Post-review code fix

--- src/lr1/Lr1.kt
+++ src/lr1/Lr1.kt
@@ -7,6 +7,7 @@ fun main() {
 
     println("Hello! My name is DICT_Bot.")
     println("Please, remind me your name.")
+    print("> ")
     val name = scanner.nextLine()
     println("What a great name you have, $name!")
 
@@ -20,8 +21,11 @@ fun main() {
 fun guessAge(scanner: Scanner) {
     println("Let me guess your age.")
     println("Enter remainders of dividing your age by 3, 5 and 7.")
+    print("> ")
     val remainder3 = scanner.nextInt()
+    print("> ")
     val remainder5 = scanner.nextInt()
+    print("> ")
     val remainder7 = scanner.nextInt()
     val age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
     println("Your age is $age!")
@@ -29,6 +33,7 @@ fun guessAge(scanner: Scanner) {
 
 fun count(scanner: Scanner) {
     println("Now I will prove to you that I can count to any number you want.")
+    print("> ")
     val num = scanner.nextInt()
     for (i in 0..num) {
         println("$i!")
@@ -43,6 +48,7 @@ fun quiz(scanner: Scanner) {
     println("3. To determine the execution time of a program.")
     println("4. To interrupt the execution of a program.")
     while (true) {
+        print("> ")
         val answer = scanner.next()
         if (answer == "2") {
             break
